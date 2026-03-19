08e8de159271b24bd34bbc04c44e300644e42d69
prasangautam7 <prasan.079bei022@tcioe.edu.np>
2026-03-20 01:08:03 +0545

    error fix take 2

--- ml_new/webapp/app.py
+++ ml_new/webapp/app.py
@@ -565,4 +565,7 @@ def upload_csv():
     return jsonify({'error': 'No CSV data received or unsupported content type'}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0', 
+        port=int(os.environ.get("PORT", 5000))
+    )
